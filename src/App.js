b89d3ae91b1ae33e9f3649ab8f2738a14262855b
rikkei-duynd <duynd@rikkeisoft.com>
2022-06-08 08:58:25 +0700

    recaptcha v2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from "react";
+import ReCaptcha from "@matt-block/react-recaptcha-v2";
 function App() {
+
+  const handleSubmit = () => {
+    console.log('ok')
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <button type='submit'>Submit</button>
+      </form>
+      <ReCaptcha
+        siteKey="6LcPi1EgAAAAAI141erLcLTPhGJyacYZW3zVaEm_"
+        theme="light"
+        size="normal"
+        onSuccess={(captcha) => console.log(`Successful, result is ${captcha}`)}
+        onExpire={() => console.log("Verification has expired, re-verify.")}
+        onError={() => console.log("Something went wrong, check your conenction")}
+      />
     </div>
   );
 }
